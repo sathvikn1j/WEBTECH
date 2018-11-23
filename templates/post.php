bef820cab39df3182755e599fbe1c3e1d96cecdb
sathvikn1j <45256712+sathvikn1j@users.noreply.github.com>
2018-11-23 17:41:37 +0530

    /images/

---
+++ templates/post.php
@@ -0,0 +1,30 @@
+<section class="post">
+	<div class="post_user">
+		<a href="#"><img class="profile_pic" src="#"/></a>
+		<a class="user_name" href="#"><h4></h4></a>
+	</div>
+	<div class="post_body">
+		<p></p>
+	</div>
+	<div class="post_img">
+		<img class="img" src="#">
+	</div>
+	<div class="post_info">
+		<table>
+			<tr>
+			<td class="likes"></td>
+			<td class="comments"></td>
+			<td class="date-time"></td>
+			</tr>
+		</table>
+	</div>
+	<div class="post_options">
+		<table>
+			<tr>
+			<td><button class="like_button">Like</button></td>
+			<td><button class="comment_button">Comment</button></td>
+			<td><button class="share_button">Share</button></td>
+			</tr>
+		</table>
+	</div>
+</section>
